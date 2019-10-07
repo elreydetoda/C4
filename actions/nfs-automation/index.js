9ce1727b1e74d05169215451c85962cd02aebf69
Lionel Faber <lionel1704@gmail.com>
2019-10-07 14:49:01 +0530

    let's do some stuff with the issues

--- actions/nfs-automation/index.js
+++ actions/nfs-automation/index.js
@@ -6,7 +6,39 @@ async function handleNewIssue() {
     const octokit = new GitHub(token);
     const context = _context;
 
-    console.log(context);
+    if (context.payload.issue == undefined) {
+        throw Error(`This action should be run against issue events.
+                    See https://help.github.com/en/articles/events-that-trigger-workflows#issues-event-issues`);
+    }
+
+    const issue = context.payload.issue;
+
+    if (!issue.title.startsWith("[NFS]") || !issue.labels.contains("NFS")) {
+        console.log("The issue is not a new NFS submission.");
+        return;
+    }
+
+    title = issue.title.slice(5);
+    var color = "000000".replace(/0/g,function(){return (~~(Math.random()*16)).toString(16);});
+
+    // Create a label with a valid description
+    await octokit.issues.createLabel({
+        ...context.payload.repository.owner,
+        ...context.payload.repository.name,
+        ...color,
+        name: title,
+        description: `specific to ${title} c2 framework`,
+    });
+
+    // Create the issue for the video
+    await octokit.issues.create({
+        ...context.payload.repository.owner,
+        ...context.payload.repository.name,
+        title: "[VIDEO] ".concat(title),
+        labels: ["video", "NFS", "enhancement", title],
+        body: "Test issue body for now",
+    });
+
 }
 
 handleNewIssue()
